a6f688931e70e08842c31f5cc1acb4ce5dba65e8
trumpocalyspe <mark.jn.kim@gmail.com>
2018-09-21 17:46:43 -0700

    added css file, bg image

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
 
-class Title extends Component {
-  render() {
-    return(
-      <div>
-        <h1>Weather Finder</h1>
-        <p>Enter your location to find current weather</p>
-
-      </div>
-    )
-  }
-};
+const Title = () => (
+  <div>
+    <h1>Weather Finder</h1>
+    <p>Enter your location to find current weather</p>
+  </div>
+);
 
 export default Title;
