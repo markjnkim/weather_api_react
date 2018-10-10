cb968c3d48e64642c33ec015efe2d194cf4ef37a
trumpocalyspe <mark.jn.kim@gmail.com>
2018-10-09 19:01:45 -0700

    added env file

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import Title from './components/Title/Title';
 import Weather from './components/Weather/Weather';
 import Form from './components/Form/Form';
 import './App.css';
-
-const API_KEY = '51120a1852d3bb845834ebad87c9648e'
-const SAN_DIEGO_ID = '3987342';
-const FORECAST_URL_SD = 'api.openweathermap.org/data/2.5/forecast?id=3987342&APPID=51120a1852d3bb845834ebad87c9648e'
+require('dotenv').config();
 
 class App extends Component {
   state = {
@@ -22,7 +19,7 @@ class App extends Component {
     event.preventDefault();
     const city = event.target.elements.city.value;
     const country = event.target.elements.country.value;
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}&units=imperial`);
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${process.env.API_KEY}&units=imperial`);
     const data = await api_call.json();
     if (city && country) {
       console.log(data);
